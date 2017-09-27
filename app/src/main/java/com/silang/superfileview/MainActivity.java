72491bb7eb638d70f9a37ecda7113583173c2665
zxw <3318767889@qq.com>
2017-09-27 10:19:10 +0800

    Initial commit

---
+++ app/src/main/java/com/silang/superfileview/MainActivity.java
@@ -0,0 +1,182 @@
+package com.silang.superfileview;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
+import android.os.storage.StorageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.silang.superfileview.R;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    private String filePath;
+    private List<String> datas = new ArrayList<>();
+    private List<String> paths = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDatas();
+        initPaths();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.mRecyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+                return new RecyclerView.ViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.item, parent, false)) {
+                };
+
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+                holder.itemView.findViewById(R.id.item_root).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String[] perms = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        filePath = getFilePath(position);
+                        if (!EasyPermissions.hasPermissions(MainActivity.this, perms)) {
+                            EasyPermissions.requestPermissions(MainActivity.this, "需要访问手机存储权限！", 10086, perms);
+                        } else {
+                            FileDisplayActivity.show(MainActivity.this, filePath);
+                        }
+                    }
+                });
+                ((TextView) holder.itemView.findViewById(R.id.item_tv)).setText(getDatas().get(position));
+            }
+
+            @Override
+            public int getItemCount() {
+                return getDatas().size();
+            }
+        });
+    }
+
+    private void initPaths() {
+    }
+
+    private void initDatas() {
+        datas.add("网络获取并打开doc文件");
+        datas.add("打开本地doc文件");
+
+
+        datas.add("打开本地txt文件");
+
+        datas.add("打开本地excel文件");
+
+
+        datas.add("打开本地ppt文件");
+
+
+        datas.add("打开本地pdf文件");
+    }
+
+    private List<String> getDatas() {
+
+        if (datas != null && datas.size() > 0) {
+            return datas;
+        } else {
+            datas = new ArrayList<>();
+            initDatas();
+            return datas;
+        }
+
+    }
+
+    private String getFilePath(int position) {
+        String path = getStoragePath(this,false);
+        switch (position) {
+            case 0:
+                path = "http://www.hrssgz.gov.cn/bgxz/sydwrybgxz/201101/P020110110748901718161.doc";
+                break;
+            case 1:
+
+                path = path +"/xiaomno.docx";
+
+                break;
+
+
+            case 2:
+                path = path+"/test.txt";
+                break;
+
+            case 3:
+                path = path+"/heihei.xlsx";
+                break;
+            case 4:
+                path = path+"/pptx.pptx";
+                break;
+
+            case 5:
+                path = path+"/jin.pdf";
+                break;
+        }
+        return path;
+    }
+    /**
+     * 通过反射调用获取内置存储和外置sd卡根路径(通用)
+     *
+     * @param mContext    上下文
+     * @param is_removale 是否可移除，false返回内部存储，true返回外置sd卡
+     * @return
+     */
+    private static String getStoragePath(Context mContext, boolean is_removale) {
+
+        StorageManager mStorageManager = (StorageManager) mContext.getSystemService(Context.STORAGE_SERVICE);
+        Class<?> storageVolumeClazz = null;
+        try {
+            storageVolumeClazz = Class.forName("android.os.storage.StorageVolume");
+            Method getVolumeList = mStorageManager.getClass().getMethod("getVolumeList");
+            Method getPath = storageVolumeClazz.getMethod("getPath");
+            Method isRemovable = storageVolumeClazz.getMethod("isRemovable");
+            Object result = null;
+            try {
+                result = getVolumeList.invoke(mStorageManager);
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            final int length = Array.getLength(result);
+            for (int i = 0; i < length; i++) {
+                Object storageVolumeElement = Array.get(result, i);
+                String path = (String) getPath.invoke(storageVolumeElement);
+                boolean removable = (Boolean) isRemovable.invoke(storageVolumeElement);
+                if (is_removale == removable) {
+                    return path;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
